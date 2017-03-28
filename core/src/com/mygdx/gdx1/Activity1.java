553938b4b3a534c98a05849dc80b0214ea0953f2
tinmarino <tinmarino@gmail.com>
2017-03-28 23:21:19 +0200

    setScreen resetsinput proc Activity1 (:= main)

--- core/src/com/mygdx/gdx1/Activity1.java
+++ core/src/com/mygdx/gdx1/Activity1.java
@@ -3,6 +3,8 @@ package com.mygdx.gdx1;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
@@ -11,15 +13,27 @@ public class Activity1 extends Game{
 	Stage stage;
 	Skin skin;
 
-
-	
 	@Override
 	public void create () {
 		Gdx.app.setLogLevel(Application.LOG_INFO);
 		Gdx.app.log("GDX1 : ", "Activity starting");
-		Tests.init();
 		this.screen = new MenuScreen(this);
 		this.screen.show();
+	}
+
+	@Override
+	public void render () {
+		super.render();
+		if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+			setScreen(new MenuScreen(this));
+		}
+	}
+
+	
+	@Override
+	public void setScreen (Screen screen) {
+		Gdx.input.setInputProcessor(null);
+		super.setScreen(screen);
 	}
 
 
