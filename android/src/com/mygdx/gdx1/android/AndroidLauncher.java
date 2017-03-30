cdd46bfc7b683c1df90e6c9d73b565d7cb291554
tinmarino <tinmarino@gmail.com>
2017-03-30 15:40:11 +0200

    Add android compatibility

--- android/src/com/mygdx/gdx1/android/AndroidLauncher.java
+++ android/src/com/mygdx/gdx1/android/AndroidLauncher.java
@@ -1,15 +1,18 @@
 package com.mygdx.gdx1.android;
 
-import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.gdx1.Activity1;
+import com.mygdx.gdx1.Tests;
+import com.mygdx.gdx1.TestsNotGwt;
+
+import android.os.Bundle;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		TestsNotGwt.fillClassList(Tests.getClassList());
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Activity1(), config);
 	}
