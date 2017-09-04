7464d011ef7041e6695a83e8b59568d8e39fe2ac
tinmarino <tinmarino@gmail.com>
2017-09-04 20:18:02 +0200

    BulletTuto2: free fall btRigitBody

--- core/src/com/mygdx/gdx1/Activity1.java
+++ core/src/com/mygdx/gdx1/Activity1.java
@@ -22,7 +22,7 @@ public class Activity1 extends Game{
 		// For android back key
 		Gdx.input.setCatchBackKey(true);
 		// this.screen = new MenuScreen(this);
-		this.screen = new BulletTuto();
+		this.screen = new BulletTuto2();
 		this.screen.show();
 	}
 
