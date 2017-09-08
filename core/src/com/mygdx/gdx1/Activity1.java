1b039daf84748af22c3771899e1c40d4e3189ca0
tinmarino <tinmarino@gmail.com>
2017-09-08 18:12:26 +0200

    CardGame 2D

--- core/src/com/mygdx/gdx1/Activity1.java
+++ core/src/com/mygdx/gdx1/Activity1.java
@@ -21,8 +21,8 @@ public class Activity1 extends Game{
 
 		// For android back key
 		Gdx.input.setCatchBackKey(true);
-		this.screen = new MenuScreen(this);
-		// this.screen = new CardGame();
+		// this.screen = new MenuScreen(this);
+		this.screen = new CardGame();
 		this.screen.show();
 	}
 
